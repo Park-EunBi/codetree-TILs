d770d8828b063a1099edeba2a962f8a0a5dbbde6
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-02-13 16:15:39 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 143ms, Memory: 27MB, Status: Passed - CodeTree

--- 240213/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240213/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,4 +1,5 @@
 n, m = map(int, input().split())
+# 0: 뱀 있, 1: 뱀 없 
 board = [
     list(map(int, input().split()))
     for _ in range(n)
@@ -9,18 +10,28 @@ def canGo(x, y):
     if x < 0 or x >= n or y < 0 or y >= m:
         return False 
     # 2. 장애물, 방문 여부 확인 
-    if board[x][y] == 1:
+    if board[x][y] == 0:
         return False 
     return True
 
 def dfs(x, y):
-    if x == n - 1 and y == m - 1:
-        return True
+    # print("dfs({}, {})".format(x, y))
+    # if x == n - 1 and y == m - 1:
+    #     return True
     
-    board[x][y] = 1 # 방문처리 
+    board[x][y] = 0 # 방문처리 
     for dxs, dys in zip((0, 1), (1, 0)):
         dx, dy = x + dxs, y + dys
         if canGo(dx, dy):
             dfs(dx, dy)
 
-print(1) if dfs(0, 0) else print(0)
+# print(1) if dfs(0, 0) else print(0)
+dfs(0, 0)
+print(0) if board[n-1][m-1] else print(1)
+
+
+'''
+2 2
+1 1
+0 1
+'''
