efd6759290fa11db1e6ff9732ee90855697fb2d0
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-02-14 20:56:37 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240214/갈 수 있는 곳들/places-can-go.py
+++ 240214/갈 수 있는 곳들/places-can-go.py
@@ -1,5 +1,7 @@
 # 입력 - 0: 이동 가능, 1: 이동 불가 
 from collections import deque 
+from sys
+input = sys.stdin.readline
 
 n, k = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
