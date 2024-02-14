3ad9d6e59014c0556ba0e8dacbac46aa8a66d7d4
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-02-14 20:59:14 +0900

    Title: 갈 수 있는 곳들, Time: 188ms, Memory: 26MB, Status: Passed - CodeTree

--- 240214/갈 수 있는 곳들/places-can-go.py
+++ 240214/갈 수 있는 곳들/places-can-go.py
@@ -12,12 +12,13 @@ cnt = 0
 
 def push(x, y):
     global cnt
-    # 1. 큐 삽입 
-    q.append((x, y))
-    # 2. 방문 처리 
-    visited[x][y] = 1
-    # 3. 숫자 세기 
-    cnt += 1
+    if not visited[x][y]:
+        # 1. 큐 삽입 
+        q.append((x, y))
+        # 2. 방문 처리 
+        visited[x][y] = 1
+        # 3. 숫자 세기 
+        cnt += 1
 
 def canGo(x, y):
     # 1. 격자 외부 
@@ -39,7 +40,16 @@ def bfs():
 # main
 for _ in range(k): 
     cx, cy = map(int, input().split())
-    q.append((cx - 1,cy - 1))
+    # q.append((cx - 1,cy - 1))
+    push(cx - 1, cy - 1)
     bfs()
 
 print(cnt)
+
+'''
+1 1
+0
+1 1
+
+1
+'''
