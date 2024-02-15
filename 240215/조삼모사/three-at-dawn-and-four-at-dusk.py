ec4f4ecdcbd93820aaa0ee87d420375788eb35b0
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-02-15 21:38:45 +0900

    Title: 조삼모사, Time: 2625ms, Memory: 29MB, Status: Passed - CodeTree

--- 240215/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240215/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -1,42 +1,52 @@
+import sys 
+INT_MAX = sys.maxsize
+
 n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
+p = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+evening = [
+    False for _ in range(n)
+]
+ans = INT_MAX
 
-choice_list = [] # 각 항목의 절반은 아침, 절반은 저녁
+# 아침과 저녁의 차이를 계산 
+def calc():
+    moring_sum = sum([
+        p[i][j]
+        for i in range(n)
+        for j in range(n)
+        if not evening[i] and not evening[j] # 저녁에 해당하지 않는다면 
+    ])
 
-# 0~total 수 중 num 개의 수를 뽑아서 ret_list에 저장하라 
-ret = []
-def choice(num, ret_list, total): 
-    if len(ret) == num:
-        ret_list.append(list(ret))    
+    evening_sum = sum([
+        p[i][j]
+        for i in range(n)
+        for j in range(n)
+        if evening[i] and evening[j]
+    ])
+
+    return abs(moring_sum - evening_sum)
+
+def find_min(curr_idx, cnt):
+    global ans
+
+    if cnt == n//2:
+        ans = min(ans, calc())
+        return 
+    
+    if curr_idx == n:
         return 
 
-    for i in range(total):
-        if i not in ret:
-            ret.append(i)
-            choice(num, ret_list, total)
-            ret.pop()
+    # curr_idx 업무를 아침에 하는 경우 
+    find_min(curr_idx + 1, cnt)
 
-choice(n, choice_list, n)
+    # curr_idx 업무를 밤에 하는 경우 
+    evening[curr_idx] = True # append 
+    find_min(curr_idx + 1, cnt + 1) # 재귀
+    evening[curr_idx] = False # pop
 
-def make_sum(cal_list, day):
-    # call list 에 담긴 인덱스
-    # 계산할 리스트를 받아서 더할 i, j를 뽑아내서 더함 
-    temp_sum = 0
-    for c in cal_list:
-        i = day[c[0]]
-        j = day[c[1]]
-        temp_sum += board[i][j]
-    return temp_sum
+find_min(0, 0)
 
-diff = float('inf')
-
-for ch in choice_list:
-    idx_list = [] 
-    morning = ch[:n//2]
-    evening = ch[n//2:]
-
-    # 각 항목당 2개씩 뽑아서 더해주기 
-    choice(2, idx_list, n//2)
-    diff = min(diff, abs(make_sum(idx_list, morning) - make_sum(idx_list, evening)))
-
-print(diff)
+print(ans)
