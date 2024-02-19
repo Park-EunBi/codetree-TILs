cfc633f77fa8097ae193ef492860ba1991e34792
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-02-19 20:09:20 +0900

    Title: 겹치지 않게 선분 고르기, Time: 185ms, Memory: 26MB, Status: Passed - CodeTree

--- 240219/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240219/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -1,3 +1,47 @@
+n = int(input())
+lines = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+ans = 0
+selected_segs = list()
+
+def overlapped(seg1, seg2):
+    (ax1, ax2), (bx1, bx2) = seg1, seg2
+    # 한 점이 다른 선분에 포함되는지 확인
+    # 아래 등식에 하나라도 포함되면 안된다  
+    return (ax1 <= bx1 and bx1 <= ax2) or (ax1 <= bx2 and bx2 <= ax2) or \
+            (bx1 <= ax1 and ax1 <= bx2) or (bx1 <= ax2 and ax2 <= bx2)
+
+def possible():
+    # 한 쌍이라도 겹치면 안됨 
+    for i, seg1 in enumerate(selected_segs):
+        for j, seg2 in enumerate(selected_segs):
+            if i < j and overlapped(seg1, seg2):
+                return False 
+    return True
+
+def find_max_segements(cnt):
+    global ans
+    if cnt == n:
+        if possible():
+            ans = max(ans, len(selected_segs))
+        return 
+
+    # 해당 라인을 포함했을 때 
+    selected_segs.append(lines[cnt])
+    find_max_segements(cnt + 1)
+    selected_segs.pop()
+
+    # 해당 라인을 포함하지 않았을 때 - append 안함
+    find_max_segements(cnt + 1)
+
+find_max_segements(0)
+print(ans)
+
+'''
+# 틀림
 import sys 
 input = sys.stdin.readline
 
@@ -23,6 +67,7 @@ def canMake(c):
 
 def choose(num):
     if num == n:
+        print(choice)
         if set(choice) not in choice_list:
             choice_list.append(set(choice))
         return 
@@ -39,6 +84,7 @@ for c in choice_list:
         maximum = max(maximum, len(c))
 
 print(maximum)
+'''
 
 '''
 7
