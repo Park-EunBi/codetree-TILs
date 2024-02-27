fa3cc26688427f9de62b0ab0872d4b61ad9fef2c
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-02-27 10:48:10 +0900

    Title: 금 채굴하기, Time: 198ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240227/금 채굴하기/gold-mining.py
@@ -0,0 +1,44 @@
+# 0: 빈 칸, 1: 금
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+
+def in_range(x, y):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False 
+    return True
+
+# 채굴 
+def get_coin(k, x, y): 
+    c = 0 # coin 
+     # 모든 범위 탐색 
+    for a in range(n):
+        for b in range(n):
+            # 마름모 범위 안에 드는지 확인 
+            if abs(a - x) + abs(b - y) <= k:
+                # 격자 내에 존재하는지 확인 
+                if in_range(a, b) and board[a][b]:
+                    # 채굴  
+                    c += 1
+    return c
+
+# 채굴 비용대비 손해를 보지 않을 수 있는지 계산 
+def calc(k, x, y):
+    # 채굴 비용 
+    need = k * k + (k + 1) * (k + 1)
+    # 코인 획득
+    coin = get_coin(k, x, y)
+
+    if coin * m >= need: # 부등호 확인 
+        return coin 
+    return 0
+
+
+# 0. k 설정
+for k in range(n):
+    # 1. 중심점 잡기 
+    for i in range(n):
+        for j in range(n):
+            ans = max(ans, calc(k, i, j))
+
+print(ans)
