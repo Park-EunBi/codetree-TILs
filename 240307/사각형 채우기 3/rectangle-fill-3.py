5093d825a5a46affc37f85eaff2f2c2190083605
Eunbi Park <87464975+Park-EunBi@users.noreply.github.com>
2024-03-07 15:42:42 +0900

    Title: 사각형 채우기 3, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240307/사각형 채우기 3/rectangle-fill-3.py
+++ 240307/사각형 채우기 3/rectangle-fill-3.py
@@ -11,7 +11,7 @@ dp[2] = 7
 #         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
 # (A + B) mod C = ((A mod C) + (B mod C)) mod C
 for i in range(3, n + 1):
-    dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) * MOD
+    dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % MOD
     for j in range(i - 3, -1, -1):
         dp[i] = (dp[i] + 2 * dp[j]) % MOD
 
